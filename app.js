562e70aa060859b3374a59191d01cfb4ba960649
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-08-12 17:35:39 -0700

    Error handler implemented

--- app.js
+++ app.js
@@ -10,15 +10,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+const AppError = require('./utils/appError');
+const errorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tours');
 const userRouter = require('./routes/users');
-
-app.all('*', (request, response, next) => {
-  response.status(404).json({
-    status: 'failed',
-    message: `Can't find ${request.originalUrl} on this server!`,
-  });
-});
 
 app.use((request, response, next) => {
   request.requestTime = new Date().toISOString();
@@ -37,5 +33,11 @@ app.get('/', (request, response) => {
     app: 'Natours',
   });
 });
+
+app.all('*', (request, response, next) => {
+  next(new AppError(`Can't find ${request.originalUrl} on this server!`, 404));
+});
+
+app.use(errorHandler);
 
 module.exports = app;
