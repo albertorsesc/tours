16af335e05acba60e45b0819fab8a3c619dc85fd
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-08-27 19:37:41 -0700

    Avoiding Mass assignment issue on store User

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,12 @@ const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (request, response, next) => {
-  const newUser = await User.create(request.body);
+  const newUser = await User.create({
+    name: request.body.name,
+    email: request.body.email,
+    password: request.body.password,
+    passwordConfirmation: request.body.passwordConfirmation,
+  });
 
   response.status(201).json({
     status: 'success',
