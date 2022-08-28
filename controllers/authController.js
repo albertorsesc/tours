f017f7efd9463574e230fca46ed8e8be49efc185
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-08-27 20:03:10 -0700

    JWT installation + JWT implementation on signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 
@@ -9,8 +10,13 @@ exports.signup = catchAsync(async (request, response, next) => {
     passwordConfirmation: request.body.passwordConfirmation,
   });
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   response.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
