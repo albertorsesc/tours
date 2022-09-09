24aa7104e408de6f68c8826622a13c5c7e77350f
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-09-09 14:30:09 -0700

    Render Tour cards in overview

--- controllers/webController.js
+++ controllers/webController.js
@@ -1,8 +1,14 @@
-exports.overview = (request, response) => {
+const Tour = require('../models/tour');
+const catchAsync = require('../utils/catchAsync');
+
+exports.overview = catchAsync(async (request, response) => {
+  const tours = await Tour.find();
+
   response.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (request, response) => {
   response.status(200).render('tour', {
